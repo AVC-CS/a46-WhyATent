2b4a1655feae0b321388dfd29420bb619daf632e
WhyATent <WWyatnt@gmail.com>
2026-03-19 20:40:49 -0700

    a46, but I had to change the data#.txt to include the inputs intended on main_test.py.

--- main.cpp
+++ main.cpp
@@ -8,24 +8,37 @@ int main()
     string status, gender, major;
     double factor = 0.1;
     double scholarship = 10000.0;
-
-    // TODO
-
-    // TODO
-        // TODO
-            // TODO
-                // TODO
-                // TODO
-            // TODO
-                // TODO
-                // TODO
-        // TODO
-            // TODO
-
-    // TODO
-
+    cout << "Input if you are student/nonstudent, gender (male/female/other), and major (cs/other): (separate by spaces, not commas)\n";
+    cin >> status >> gender >> major;
+    if (status == "student"){
+        if (gender =="female"){
+            if (major =="cs"){
+                factor=3.0;
+                
+            }
+            else{
+                factor=1.0;
+            }
+        }
+        else{
+            if (major =="cs"){
+                factor=2.0;
+            }
+            else{
+                factor=0.2;
+            }
+        }
+    }
+    else{
+        if (major =="cs"){
+            factor=0.5;
+        }
+        else{
+            factor=0.1; //The default factor.
+        }
+    }
     cout << setprecision(2) << fixed;
-    // TODO
-
+    double amount = scholarship*factor;
+    cout << "factor=" << factor <<  ", amount=" << amount << endl;
     return 0;
 }
